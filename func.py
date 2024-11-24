10f79275807f5b59f9efeea284a54d6cd51e6a7e
darylangg <darylang96@hotmail.com>
2024-11-24 21:57:38 +0800

    initial commit

---
+++ func.py
@@ -0,0 +1,18 @@
+from parliament import Context, event
+
+
+@event
+def main(context: Context):
+    """
+    Function template
+    The context parameter contains the Flask request object and any
+    CloudEvent received with the request.
+    """
+
+    # Add your business logic here
+
+    # The return value here will be applied as the data attribute
+    # of a CloudEvent returned to the function invoker
+    data = context.cloud_event.data
+    data["collection_name"] = "comments"
+    return data
